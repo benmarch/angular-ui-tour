be5c486f77b9a86463aeff1d6e70508750dc5361
Ben March <bmarch89@gmail.com>
2016-03-01 19:36:23 -0500

    Added live tour demo

--- scripts/angular-ui-tour-demo.js
+++ scripts/angular-ui-tour-demo.js
@@ -1,1 +1,49 @@
-angular.module('uiTourDemo', ['bm.uiTour']);
+angular.module('uiTourDemo', ['bm.uiTour', 'ngRoute'])
+
+    .config(['TourConfigProvider', '$routeProvider', function (TourConfigProvider, $routeProvider) {
+
+        TourConfigProvider.set('scrollOffset', 200);
+
+        TourConfigProvider.set('onStart', function () {
+            console.log('Started Tour');
+        });
+        TourConfigProvider.set('onNext', function () {
+            console.log('Moving on...');
+        });
+
+
+        $routeProvider
+            .when('/docs', {
+                templateUrl: '/partials/docs.html'
+            })
+            .when('/other', {
+                templateUrl: '/partials/other.html'
+            })
+            .otherwise({
+                redirectTo: '/docs'
+            });
+
+    }])
+
+    .controller('TourDemoController', ['$scope', '$q', '$timeout', function ($scope, $q, $timeout) {
+        $scope.isEnabled = true;
+
+        $scope.toggleEnabled = function () {
+            $scope.isEnabled = !$scope.isEnabled;
+        };
+
+        $scope.onPrev = function (tour) {
+            console.log('Moving back...', tour);
+        };
+
+        $scope.confirmShow = function () {
+            return $q(function (resolve) {
+                if (confirm('Click OK if you don\'t see the popup, and 1 second later you will!')) {
+                    $timeout(resolve, 1000);
+                } else {
+                    resolve();
+                }
+            });
+        };
+
+    }]);
